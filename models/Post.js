2503d178e9b9c7864654327fcea61541607f36c9
Bruno Makoto Tanabe de Lima <bruno.makoto04@gmail.com>
2023-06-25 23:52:00 -0300

    authentication

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+// This is a mongoose model that allows me to perform CRUD operations to the 'Post' collection on my Mongo db
+
+const { model, Schema } = require('mongoose');
+
+const postSchema = new Schema({ 
+    body: String,
+    username: String,
+    createdAt: String,
+    comments: [
+        {
+            body: String,
+            username: String,
+            createdAt: String
+        }
+    ],
+    likes: [
+        {
+            username: String,
+            createdAt: String
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
+});
+
+module.exports = model('Post', postSchema);
